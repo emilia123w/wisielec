8ae34ee2b46e8f0599efbfe10c7e7346fea10e9b
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-12-23 00:39:42 +0100

    game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-var haslo ="bez pracy nie ma kolaczy";
+var haslo ="bez pracy nie ma kołaczy";
 haslo=haslo.toUpperCase();
 var dlugosc = haslo.length;
+var ileSkuch=0;
 
 var haslo1 = "";
 
@@ -56,11 +57,60 @@ function start(){
   var tresc_diva="";
 
   for(i=0; i<35; i++){
-    tresc_diva= tresc_diva+'<div class="litera">'+litery[i]+'</div>'
+    var element = "lit"+i;
+    tresc_diva= tresc_diva+'<div class="litera" onclick="sprawdz('+i+')" id="'+element+'">'+litery[i]+'</div>'
     if((i+1)%7==0) tresc_diva = tresc_diva+ '<div style="clear:both;"></div>'
   }
 
   document.getElementById("alfabet").innerHTML=tresc_diva;
 
   wypisz_haslo();
+}
+
+String.prototype.ustawZnak=function(miejsce,znak){
+  if(miejsce>this.length-1)return this.toString();
+  else return this.substr(0,miejsce)+znak+this.substr(miejsce+1);
+}
+
+
+function sprawdz(nr){
+  var trafiona = false;
+  for (i=0; i<dlugosc; i++){
+    if(haslo.charAt(i)==litery[nr]){
+      haslo1=haslo1.ustawZnak(i,litery[nr]);
+      trafiona=true;
+    }
+
+  }
+  if(trafiona==true){
+    var element = "lit"+nr;
+    document.getElementById(element).style.background="#003300";
+    document.getElementById(element).style.color="#00C000";
+    document.getElementById(element).style.border="3px solid #00C000";
+    document.getElementById(element).style.cursor="default";
+    wypisz_haslo();
+  }
+  else{
+    var element = "lit"+nr;
+    document.getElementById(element).style.background="#330000";
+    document.getElementById(element).style.color="#C00000";
+    document.getElementById(element).style.border="3px solid #C00000";
+    document.getElementById(element).style.cursor="default";
+    document.getElementById(element).setAttribute("onclick",";");
+    ileSkuch++;
+    var obraz = "img/s"+ileSkuch+".png";
+
+    document.getElementById("obrazek").innerHTML='<img src="'+obraz+'"alt=""/>'
+
+
+
+  }
+  //wygrana
+  if(haslo==haslo1)
+  document.getElementById("alfabet").innerHTML="tak jest! Podano prawidlowe haslo:"+haslo+'<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>'
+  //przegrana
+  if(ileSkuch>=5)
+  document.getElementById("alfabet").innerHTML="Przegrana! Prawidlowe haslo:"+haslo+'<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>'
+
+  
 }
